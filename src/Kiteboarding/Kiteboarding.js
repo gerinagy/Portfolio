79979d30f06f65c96aa8fad0e5008e903c0244ac
gerinagy <nagy.gergi6@gmail.com>
2020-11-24 21:56:20 +0100

    firebase

--- src/Kiteboarding/Kiteboarding.js
+++ src/Kiteboarding/Kiteboarding.js
@@ -16,10 +16,37 @@ function Kiteboarding() {
       </section>
 
       <section className="k-teaching-wrapper">
-        <div className="content-wrapper">
-          <h3>teacxin</h3>
-          <img src="../image/desert.jpg" alt=""/>
+
+        <div className="k-teaching-text">
+          <h3>Teaching</h3>
+          <p>Feel free to contact about Kitelessons and details.</p>
+          <a name="Contact me." id="" class="btn btn-primary" href="#" role="button"></a>
         </div>
+
+        <div className="k-kite-image"></div>
+
+      </section>
+
+      <section className="k-eleveight-wrapper">
+        <div className="k-eleveight-imgtext">
+          <div className="k-eleveight-image"></div>
+          <h3>2020 FS</h3>
+        </div>
+
+
+        <div className="k-eleveight-text">
+
+          <h3>Eleveight Kites</h3>
+          <p>Kites. Boards. Other Equipments.
+            <br></br>Check the website and contact me.
+          </p>
+          <div className="k-buttons">
+            <a href="https://www.eleveightkites.com" target="_blank" rel="noopener noreferrer"><button>Visit website</button></a>
+
+            <a href="https://www.eleveightkites.com" target="_blank" rel="noopener noreferrer"><button>contact me</button></a>
+          </div>
+        </div>
+
 
 
       </section>
