a53973dc4c79f28262c2473e48d1d7ddda644f25
gerinagy <nagy.gergi6@gmail.com>
2020-12-09 23:56:00 +0100

    burger

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -26,65 +26,66 @@ const Burger = () => {
       </div>
       <div className={open ? "side-backdrop active" : "side-backdrop"} onClick={handleClick}></div>
       <aside className={open ? "nav-sidebar active" : "nav-sidebar"}>
+        <div className="sidebar-wrapper">
+          <ul>
+            <li onClick={() => setOpen(false)}><Link to="/" className="menulinks">Home</Link></li>
+            <li onClick={() => setOpen(false)}><Link to="/Kiteboarding" className="menulinks">Kiteboarding</Link></li>
+            <li onClick={() => setOpen(false)}><Link to="/Webdevelopment" className="menulinks">Web Development</Link></li>
+            <li onClick={() => setOpen(false)}><Link to="/Portfolio" className="menulinks">Portfolio</Link></li>
 
-        <ul>
-          <li onClick={() => setOpen(false)}><Link to="/" className="menulinks">Home</Link></li>
-          <li onClick={() => setOpen(false)}><Link to="/Kiteboarding" className="menulinks">Kiteboarding</Link></li>
-          <li onClick={() => setOpen(false)}><Link to="/Webdevelopment" className="menulinks">Web Development</Link></li>
-          <li onClick={() => setOpen(false)}><Link to="/Portfolio" className="menulinks">Portfolio</Link></li>
+          </ul>
+          <div className="profile">
+            <div className="profile-wrapper">
+              <div className="profilePic">
+                <img src={Profpic} alt="" />
+              </div>
 
-        </ul>
-        <div className="profile">
-          <div className="profile-wrapper">
-            <div className="profilePic">
-              <img src={Profpic} alt="" />
-            </div>
+              <div className="profileBio">
+                <h1>Hi !</h1>
+                <p>Welcome to my Portfolio! This Website briefly present what I'm doing. You can find details about Kiteboarding, Webdevelopment and watch my photos.</p>
 
-            <div className="profileBio">
-              <h1>Hi !</h1>
-              <p>Welcome to my Portfolio! This Website briefly present what I'm doing. You can find details about Kiteboarding, Webdevelopment and watch my photos.</p>
+              </div>
 
-            </div>
+              <div className="contactinfo">
+                <div>
+                  <h1>Contact</h1>
 
-            <div className="contactinfo">
-              <div>
-                <h1>Contact</h1>
+                  <div className="profile-social-wrapper">
 
-                <div className="profile-social-wrapper">
+                    <div className="profile-social">
 
-                  <div className="profile-social">
+                      <a href="mailto:nagy.gergi6@gmail.com"><i class="fa fa-envelope"></i></a>
 
-                    <a href="mailto:nagy.gergi6@gmail.com"><i class="fa fa-envelope"></i></a>
-
-                    <a href="https://github.com/gerinagy" target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i>
-                    </a>
-
-
-                    <a to="https://api.whatsapp.com/send?phone=+36%2030%204764%20552" target="_blank" rel="noopener noreferrer"><i className="fa fa-whatsapp" aria-hidden="hidden"></i></a>
+                      <a href="https://github.com/gerinagy" target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i>
+                      </a>
 
 
+                      <a to="https://api.whatsapp.com/send?phone=+36%2030%204764%20552" target="_blank" rel="noopener noreferrer"><i className="fa fa-whatsapp" aria-hidden="hidden"></i></a>
+
+
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h1>Social Media</h1>
+                  <div className="profile-social-wrapper">
+                    <div className="profile-social">
+
+
+
+                      <a href="https://facebook.com/gergo.nagy.545/" target="_blank" rel="noopener noreferrer"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+
+                      <a href="https://instagram.com/ngeri6/" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram" aria-hidden="true"></i>
+                      </a>
+
+                      <a href="https://www.youtube.com/channel/UCXUUylIJiLE_uBLszlAqNAA" target="_blank" rel="noopener noreferrer"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+
+                    </div>
                   </div>
                 </div>
               </div>
-              <div>
-                <h1>Social Media</h1>
-                <div className="profile-social-wrapper">
-                  <div className="profile-social">
 
-
-
-                    <a href="https://facebook.com/gergo.nagy.545/" target="_blank" rel="noopener noreferrer"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-
-                    <a href="https://instagram.com/ngeri6/" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram" aria-hidden="true"></i>
-                    </a>
-
-                    <a href="https://www.youtube.com/channel/UCXUUylIJiLE_uBLszlAqNAA" target="_blank" rel="noopener noreferrer"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-
-                  </div>
-                </div>
-              </div>
             </div>
-
           </div>
         </div>
       </aside>
